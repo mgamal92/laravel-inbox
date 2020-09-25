0f7058d503afabc825ab614157332d7f2b840605
Mohamed Gamal <mg.dev1992@gmail.com>
2020-09-25 02:18:08 +0200

    init configs

---
+++ src/InboxServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace MG\Inbox;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class InboxServiceProvider.
+ */
+class InboxServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Config
+        $this->publishes([
+            __DIR__.'/../../config/laravel-inbox.php' => config_path('laravel-inbox.php'),
+        ]);
+    }
+}
